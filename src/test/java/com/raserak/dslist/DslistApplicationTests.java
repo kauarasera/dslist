7a2839d61c4fe1fa8aee588b09696c22f8a18417
Kauã Rasera <kauarasera@gmail.com>
2023-11-14 18:16:33 -0300

    Project created

---
+++ src/test/java/com/raserak/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raserak.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
